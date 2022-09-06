e4b8dfdcb93f5b5bf3ef5ac8e09e4a735ea84aa4
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-06 17:07:52 -0300

    Cria relacionamento ManyToOne entre as entidades

--- game_store/src/main/java/org/generation/gameStore/model/Jogo.java
+++ game_store/src/main/java/org/generation/gameStore/model/Jogo.java
@@ -4,8 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_jogos")
@@ -20,6 +23,18 @@ public class Jogo {
 	
 	@NotNull
 	private int preço;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("jogo")
+	private Categoria categoria;
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 
 	public Long getId() {
 		return id;
