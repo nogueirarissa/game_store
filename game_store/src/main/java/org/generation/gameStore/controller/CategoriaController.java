9a0e76ec6a5e7bd3fbf06c8021c470dad4bd9853
nogueirarissa <larissa.nogueiradm@gmail.com>
2022-09-06 16:50:42 -0300

    Cria os Recursos Categoria e Jogo na camada controller

---
+++ game_store/src/main/java/org/generation/gameStore/controller/CategoriaController.java
@@ -0,0 +1,62 @@
+package org.generation.gameStore.controller;
+
+import java.util.List;
+
+import org.generation.gameStore.model.Categoria;
+import org.generation.gameStore.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "*")
+public class CategoriaController {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> getById(@PathVariable Long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/estilo/{estilo}")
+	public ResponseEntity<List<Categoria>> getByEstilo(@PathVariable String estilo){
+		return ResponseEntity.ok(repository.findAllByEstiloContainingIgnoreCase(estilo));
+	}
+	
+	@GetMapping("/genero/{genero}")
+	public ResponseEntity<List<Categoria>> getByGenero(@PathVariable String genero){
+		return ResponseEntity.ok(repository.findAllByGeneroContainingIgnoreCase(genero));		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> post(@RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Categoria> put(@RequestBody Categoria categoria){
+		return ResponseEntity.ok(repository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
+}
